ddd94173193d28b756aed44e4470ac975a8e8d28
Aadarsh <aadarshjha6783@gmail.com>
2023-10-07 21:54:38 +0530

    Config

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
-/**
- * const config={
+ const config={
     "SERVER_URL":"https://chatbot-qa-backend.purplepebble-adb93c94.eastus.azurecontainerapps.io"
 }
- */
-const config={
+
+/**
+ * const config={
     "SERVER_URL":"http://localhost:5000"
 }
+ */
 config.CATEGORY_MAP = {
     1:"try",
     2:"fee",
