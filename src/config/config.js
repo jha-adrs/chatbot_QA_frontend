ddc3ced9d703f9c03be5a997607840eb42894917
Aadarsh Jha <88365910+jha-adrs@users.noreply.github.com>
2023-10-12 20:12:36 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,16 @@
 // const config={
 //     "SERVER_URL":"https://chatbot-qa-backend.purplepebble-adb93c94.eastus.azurecontainerapps.io"
 //  }
- const config = {
-     SERVER_URL: "https://chatbot-qa-backend.vercel.app"
- }
+ //const config = {
+ //    SERVER_URL: "https://chatbot-qa-backend.vercel.app"
+ //}
 //const config = {
   //  SERVER_URL:"http://localhost:5000"
 //}
+const config ={
+ SERVER_URL:"https://cairns-brown-snake-xtzt.1.sg-1.fl0.io"
+}
+
 config.CATEGORY_MAP = {
     1:"try",
     2:"fee",
