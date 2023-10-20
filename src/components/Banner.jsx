da2edb02ec00481e8c16a4bba96d612f5490c64c
Aadarsh <aadarshjha6783@gmail.com>
2023-10-20 11:44:07 +0530

    Chat link

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = ({close,...props}) => {
             </div>
             <div className="flex items-center flex-shrink-0">
 
-                <Link href="/chat" className="inline-flex items-center justify-center px-3 py-2 mr-2 text-xs font-medium text-white bg-primary-500 rounded-lg hover:bg-primary-700 focus:ring-4 focus:ring-gray-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-gray-800">Get started <svg className="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <Link  to="/chat" className="inline-flex items-center justify-center px-3 py-2 mr-2 text-xs font-medium text-white bg-primary-500 rounded-lg hover:bg-primary-700 focus:ring-4 focus:ring-gray-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-gray-800">Get started <svg className="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                 </svg>
                 </Link>
